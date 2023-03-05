90a05e81dca635b9c9a289c4ea17682dbb12c09d
Antonio <avps2@aluno.ifal.edu.br>
2023-03-05 14:24:37 -0300

    tela de login + tela principal
    05/03/2023

---
+++ Javascript/Tela_login.js
@@ -0,0 +1,23 @@
+let inputEmail = document.getElementById("inputEmail");
+let inputSenha = document.getElementById("inputSenha");
+
+
+function fazerLogin(){
+    let email = inputEmail.value; 
+    let senha = inputSenha.value;
+
+    if(email === "" || senha === ""){
+        alert("Por Favor Preencher Dados!");
+        inputEmail.focus();
+        return;
+    }
+
+    if(email != "aluno.ifal@ifal.edu.br" && senha != "aluno.ifal")
+    {
+        alert("Email ou Senha esta(ão) Incorreto.")
+        inputEmail.focus();
+        return;
+    }
+
+    window.location.href = "../Paginas/Tela_Principal.html";
+}
